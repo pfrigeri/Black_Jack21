4514b57241e8bf57bde60cbf66e38f3d22203fb2
pfrigeri <asockmf@gmail.com>
2024-08-06 12:10:38 -0300

    Adição de Laço para tornar o programa continuo

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -1,10 +1,5 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-
-
 def caesar(message, shift, encode_or_decode):
     if encode_or_decode == "decode":
         shift *= -1
@@ -16,9 +11,18 @@ def caesar(message, shift, encode_or_decode):
             output += alphabet[new_index] # que o tamanho da lista o módulo é calculado garantindo uma rotação circular entre a lista.
         else:
             output += letter #caso o caractere não esteja no alfabeto ele será diretamente adicionado. Isso garante a inclusão de espaços 
-    print(f"This is the encoded message: {output}")
+    print(f"This is the {encode_or_decode}d message: {output}")
 
-caesar(text, shift, direction)
+while True: #Uma Flag poderia ter sido usada também.
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+
+    caesar(text, shift, direction)
+    answer = input("Would you like to continue?: (yes/no)").lower()
+    if answer == "no":
+        print("See you later than !")
+        break
 
 
 
