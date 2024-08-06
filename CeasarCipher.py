ce1ed5e402dcd0b8b501f1c26d3f2fa23c006412
pfrigeri <asockmf@gmail.com>
2024-08-06 10:00:34 -0300

    Função descriptografar

--- CeasarCipher.py
+++ CeasarCipher.py
@@ -1,20 +1,38 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 
-def encrypt(text, shift):
+def encrypt(original_text, shift):
     encoded = ""
-    for letter in text: #Pega cada letra na mensagem
+    for letter in original_text: #Pega cada letra na mensagem
         if letter in alphabet: #Verifica se a letra está no alfabeto
             index = alphabet.index(letter) #Busca o index da letra caso estiver
             new_index = (index + shift) % len(alphabet) #Soma o Index ao número de posições a serem deslocdas e se o novo index for maior
             encoded += alphabet[new_index] # que o tamanho da lista o módulo é calculado garantindo uma rotação circular entre a lista.
         else:
             encoded += letter #caso o caractere não esteja no alfabeto ele será diretamente adicionado. Isso garante a inclusão de espaços 
-    return encoded
+    print(f"This is the encoded message: {encoded}")
             
+def dencrypt(text, shift):
+    decoded =""
+    for letter in text:
+        if letter in alphabet:
+            index = alphabet.index(letter)
+            new_index = (index - shift) % len(alphabet)
+            decoded += alphabet[new_index]
+        else:
+            decoded += letter
+    print(f"This is the encoded message: {decoded}")
 
-print(encrypt(text,shift))
+def caesar(message, shift, direction):
+    if direction == "encode":
+        encrypt(message, shift)
+    elif direction == "decode":
+        dencrypt(message, shift)
+
+caesar(text, shift, direction)
+
+
 
