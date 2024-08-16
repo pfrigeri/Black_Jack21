93619f3c0bcfc7050e71e17b6d5b006971dcf494
pfrigeri <asockmf@gmail.com>
2024-08-16 11:44:14 -0300

    Primeiro commit

---
+++ BlackJack.py
@@ -0,0 +1,7 @@
+#Criar uma lista contendo todas as possíveis cartas a serem sorteadas para os jogadores
+     #Mostrar as cartas sorteadas
+     #Perguntar se o jogador deseja sortear mais cartas
+#Somar as cartas compondo a pontuação dos jogadores
+    #Se a pontuação passar de 21 o jogador perde
+
+#Perguntar se o jogador deseja jogar novamente (deixando o jogo em laço)
